a964940f7ffea6e2e867424c5e53b34a2aaefc08
harry-c26 <>
2023-11-29 09:43:06 +0000

    Added and tested username input

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
+import random
+
 # Welcome to this Mental Maths practise game.
-# You can choose to test yourself with addition, subtraction, multiplication or division. 
+# You can choose to test yourself with addition,
+# subtraction, multiplication or division.
 # There will be 5 questions to answer to test your Mental Maths skills.
-# Your score will appear at the end. 
+# Your score will appear at the end.
 # Are you ready? Let's go!
 
 
+def get_username():
+    """
+    Get users name to personalise the quiz
+    """
+    username = input("Enter your name: ")
+    print(f"{username}, welcome to Mental Maths!")
+
+
+get_username()
