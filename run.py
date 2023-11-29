c8100837f95364271d357df6de28d1958a27ce65
harry-c26 <>
2023-11-29 09:12:53 +0000

    Added welcome text ready for the layout

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Welcome to this Mental Maths practise game.
+# You can choose to test yourself with addition, subtraction, multiplication or division. 
+# There will be 5 questions to answer to test your Mental Maths skills.
+# Your score will appear at the end. 
+# Are you ready? Let's go!
+
+
