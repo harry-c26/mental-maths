c59d517338a3e2dcdb6dd04cdd094534e3783241
harry-c26 <>
2023-11-30 11:02:54 +0000

    Removed whitespaces

--- run.py
+++ run.py
@@ -10,7 +10,9 @@ import random
 
 def get_username():
     """
-    Get users name to personalise the quiz.
+    Allows users to enter their name to personalise the quiz.
+    Validates the input to make sure something is entered before
+    adding the username to the welcome message.
     """
     username = input("Enter your name: ")
     while username == "":
@@ -19,4 +21,18 @@ def get_username():
     return username
 
 
-get_username()
+def choose_operator():
+    """
+    Gives the user the choice of which operand they want to use
+    for the quiz.
+    Validates the input to ensure it is one of the 4 options.
+    """
+    print("Please choose your operator for the quiz.")
+    print("The options are add, subtract, multiply & divide.")
+    operators = ['add', 'subtract', 'multiply', 'divide']
+    user_operator = ""
+    user_operator = input("Please enter your operator of choice: ")
+    while user_operator not in operators:
+        print("Not a valid option.")
+        user_operator = input("Please enter a valid operator: ")
+    return user_operator
